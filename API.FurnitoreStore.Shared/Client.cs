0544dbf146498388d167b0ed48ca7e85b1f1d438
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-11-04 17:17:46 -0300

    first commit

---
+++ API.FurnitoreStore.Shared/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.FurnitoreStore.Shared
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+
+        public DateTime BirhtDate { get; set; }
+        public string Phone {  get; set; }
+        public string Address {  get; set; }
+
+    }
+}
