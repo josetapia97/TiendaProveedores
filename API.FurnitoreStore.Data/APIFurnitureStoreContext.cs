561b3ae726c2bae286733e13a49b6dc11407fda0
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-11-04 21:28:34 -0300

    Capa datos: creada y conectada

---
+++ API.FurnitoreStore.Data/APIFurnitureStoreContext.cs
@@ -0,0 +1,26 @@
+﻿using API.FurnitoreStore.Shared;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.FurnitoreStore.Data
+{
+    public class APIFurnitureStoreContext : DbContext
+    {
+        //constructor para que toda la config de EF se realice
+        public APIFurnitureStoreContext(DbContextOptions options) : base(options) { }
+
+
+        //DB set: representaciones de la tabla en codigo || utiliza shared
+        public DbSet<Client> Clients {  get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        //al utsar SQLite se usa este override
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+
+    }
+}
