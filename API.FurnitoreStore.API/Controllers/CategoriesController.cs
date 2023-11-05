b754655c495ee668d0df0db4caab19fc450462d3
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-11-05 20:51:11 -0300

    Clients y Categories Controllers Created

---
+++ API.FurnitoreStore.API/Controllers/CategoriesController.cs
@@ -0,0 +1,62 @@
+﻿using API.FurnitoreStore.Data;
+using API.FurnitoreStore.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FurnitoreStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        //DB context comunication
+        private readonly APIFurnitureStoreContext _context;
+
+        //inyeccion de dependencia en el ctor
+        public CategoriesController(APIFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProductCategory>> Get()
+        {
+            return await _context.ProductCategories.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var categories = await _context.ProductCategories.FirstOrDefaultAsync(c => c.Id == id);
+            if(categories == null) return NotFound();
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ProductCategory category)
+        {
+            await _context.ProductCategories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Post",category.Id,category);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Put(ProductCategory category)
+        {
+            _context.ProductCategories.Update(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(ProductCategory category)
+        {
+            if (category == null) return NotFound();
+            _context.ProductCategories.Remove(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ API.FurnitoreStore.API/Controllers/ClientsController.cs
@@ -0,0 +1,62 @@
+﻿using API.FurnitoreStore.Data;
+using API.FurnitoreStore.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FurnitoreStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        //DB context comunication
+        private readonly APIFurnitureStoreContext _context;
+
+        //inyeccion de dependencia en el ctor
+        public ClientsController(APIFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Client>> Get()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        // la ruta de abajo seria Clients/id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var client = await _context.Clients.FirstOrDefaultAsync(c => c.Id == id);
+            if (client == null) return NotFound();
+            return Ok(client);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Client client)
+        {
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Post", client.Id,client);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Client client)
+        {
+            _context.Clients.Update(client);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Client client)
+        {
+            if(client == null) return NotFound();
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
