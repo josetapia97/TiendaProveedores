ceec5536906dec6673c4a354af90994c418c1102
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-11-08 14:02:19 -0300

    Register Endpoint: created

---
+++ API.FurnitoreStore.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,76 @@
+﻿using API.FurnitoreStore.API.Configuration;
+using API.FurnitoreStore.Shared.Auth;
+using API.FurnitoreStore.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace API.FurnitoreStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtConfig _jwtConfig;
+
+        //IOptions trae elementos desde cofig especificando el tipo de output
+        public AuthenticationController(UserManager<IdentityUser> userManager, IOptions<JwtConfig> jwtConfig)
+        {
+            _userManager = userManager;
+            _jwtConfig = jwtConfig.Value;
+        }
+
+        public async Task<IActionResult> Register([FromBody] UserRegistrationRequestDto request)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            //Verificar si el email existe
+            var emailExist = await _userManager.FindByEmailAsync(request.EmailAdress);
+            if (emailExist != null)
+                return BadRequest(new AuthResult()
+                {
+                    Result = false,
+                    Errors = new List<string>()
+                    {
+                        "Email already exists"
+                    }
+                });
+            //crear usuario
+            var user = new IdentityUser()
+            {
+                Email = request.EmailAdress,
+                UserName = request.EmailAdress
+            };
+
+            var isCreated = await _userManager.CreateAsync(user);
+            if (isCreated.Succeeded)
+            {
+                var token = GenerateToken(user);
+                return Ok(new AuthResult()
+                {
+                    Result = true,
+                    Token = token
+                });
+            }
+            else
+            {
+                var errors = new List<string>();
+                foreach (var err in isCreated.Errors)
+                    errors.Add(err.Description);
+
+                return BadRequest(new AuthResult
+                {
+                    Result= false,
+                    Errors = errors
+                });
+            }
+
+            return BadRequest(new AuthResult
+            {
+                Result = false,
+                Errors = new List<string> { "User couldn't be created" }
+            });
+        }
+    }
+}
